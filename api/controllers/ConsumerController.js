b1eac79d676cd79b4ec0b06144e4700c3450bb48
James Marlowe <jameskmarlowe@gmail.com>
2017-04-02 12:21:43 -0500

    customers are consumers

---
+++ api/controllers/ConsumerController.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+  schemaform: function (req, res) {
+    return res.send(200, {
+      "formId": "com.cemaritan.app.consumer.create",
+      "version": 1,
+      "action": [
+        {
+          "category": "consumer",
+          "name": "createconsumer",
+          "readOnly": false,
+          "title": "Create"
+        }
+      ],
+      "schema": {
+        "type": "object",
+        "title": "Find/Create Consumer",
+        "required": [
+          "firstName",
+          "lastName",
+          "ssn",
+          "dateOfBirth"
+        ],
+        "properties": {
+          "firstName": {
+            "title": "First Name",
+            "type": "string"
+          },
+          "lastName": {
+            "title": "Last Name",
+            "type": "string"
+          },
+          "ssn": {
+            "title": "Social Security Number",
+            "type": "string"
+          },
+          "dateOfBirth": {
+            "title": "Date Of Birth",
+            "type": "date"
+          }
+        }
+      },
+      "form": [
+        "firstName",
+        "lastName",
+        "ssn",
+        "dateOfBirth"
+      ]
+    });
+  }
+
+};
