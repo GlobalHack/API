7c824525ffb8c8c66a9f4076872c5f2275a24cbd
Brian Loyal <blooyal@gmail.com>
2017-04-08 22:46:59 -0500

    Updated Consumer test

--- test/unit/models/Consumer.test.js
+++ test/unit/models/Consumer.test.js
@@ -1,27 +1,32 @@
 'use strict'
 let expect = require('chai').expect
-const uid = require('uid')
-const uid1 = uid()
-const uid2 = uid()
+// const uid = require('uid')
+// const uid1 = uid()
+// const uid2 = uid()
+const faker = require('faker')
 
 describe('ConsumerModel', function() {
-  let testID = null
+  let testObject = null
+  let testFirstName = faker.fake("{{name.firstName}}")
+  let testLastName = faker.fake("{{name.lastName}}")
+
   describe('#create()', function() {
     it('should check create function', function(done) {
       // Create a new resource
       Consumer.create({
-          'firstName': uid1,
-          'lastName': uid1,
-          'ssn': uid1,
+          'firstName': testFirstName,
+          'lastName': testLastName,
+          'ssn': '123-45-6789',
           'domesticViolence': true,
           'youth': true,
           'dateOfBirth': '01-01-2001'
         })
         .then(function(results) {
           // run some tests
-          expect(results.firstName).to.equal(uid1)
+          expect(results.firstName).to.equal(testFirstName)
+          expect(results.lastName).to.equal(testLastName)
           // save the id of the new record
-          testID = results.id
+          testObject = results
           done()
         })
         .catch(done)
@@ -31,11 +36,11 @@ describe('ConsumerModel', function() {
   describe('#findOne()', function() {
     it('should check find one function', function(done) {
       Consumer.findOne({
-          'id': testID
+          'id': testObject.id
         })
         .then(function(results) {
           // run some tests
-          expect(results.firstName).to.equal(uid1)
+          expect(results.firstName).to.equal(testFirstName)
           done()
         })
         .catch(done)
@@ -45,13 +50,13 @@ describe('ConsumerModel', function() {
   describe('#update()', function() {
     it('should check update function', function(done) {
       Consumer.update({
-          'id': testID
+          'id': testObject.id
         }, {
-          'ssn': uid2
+          'ssn': 'new value'
         })
         .then(function(results) {
           // run some tests
-          expect(results[0].ssn).to.equal(uid2)
+          expect(results[0].ssn).to.equal('new value')
           done()
         })
         .catch(done)
@@ -61,11 +66,11 @@ describe('ConsumerModel', function() {
   describe('#destroy()', function() {
     it('should check destroy function', function(done) {
       Consumer.destroy({
-          'id': testID
+          'id': testObject.id
         })
         .then(function() {
           Consumer.findOne({
-              'id': testID
+              'id': testObject.id
             })
             .then(function(results) {
               expect(results).to.be.undefined
