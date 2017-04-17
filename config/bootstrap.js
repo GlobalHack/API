168def9e9b93f35aff22029c06f42c3ff1804589
James Marlowe <james.marlowe@daugherty.com>
2017-04-17 15:25:01 -0500

    more fixtures

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,11 +8,12 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-
+var _ = require('lodash');
 var question = require('../api/fixtures/Question');
 var organization = require('../api/fixtures/Organization');
 var prefix = require('../api/fixtures/Prefix');
 var menuitem = require('../api/fixtures/MenuItem');
+var role = require('../api/fixtures/Role');
 
 function installFixture(model, name, fixtures) {
   model.findOrCreateEach(fixtures, fixtures).exec(function (err, results) {
@@ -24,11 +25,27 @@ function installFixture(model, name, fixtures) {
   });
 }
 
+function createModels() {
+  var models = _.compact(_.map(sails.models, function (model, name) {
+    return model && model.globalId && model.identity && {
+        name: model.globalId,
+        identity: model.identity,
+        attributes: _.omit(model.attributes, _.functions(model.attributes))
+      };
+  }));
+
+  _.map(models, function (model) {
+    return sails.models.model.findOrCreate({ name: model.name }, model);
+  });
+}
+
 function installFixtures() {
   installFixture(Question, 'Questions', question.fixtures);
   installFixture(Organization, 'Organizations', organization.fixtures);
   installFixture(Prefix, 'Prefixes', prefix.fixtures);
   installFixture(MenuItem, 'MenuItems', menuitem.fixtures);
+  installFixture(Role, 'Roles', role.fixtures);
+  createModels();
 }
 
 module.exports.bootstrap = function (cb) {
--- config/policies.js
+++ config/policies.js
@@ -26,7 +26,16 @@ module.exports.policies = {
    *                                                                          *
    ***************************************************************************/
 
-  '*': ['TokenPolicy', 'AuthPolicy'],
+  '*': [
+    'TokenPolicy',
+    'AuthPolicy',
+    'ModelPolicy',
+    'AuditPolicy.js',
+    'OwnerPolicy',
+    'PermissionPolicy',
+    'RolePolicy',
+    'CriteriaPolicy'
+  ],
 
   UserController: ['UserPolicy'],
 
