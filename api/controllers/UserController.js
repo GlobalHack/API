cf7d4f106735526aa9a7a15ab6f5219426065a08
Darius Calliet <calliet.d@gmail.com>
2017-05-28 09:43:55 -0500

    user model should populate on signup

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,12 +2,19 @@ module.exports = {
 
   signUp: function(req, res){
     const options = {user: req.param('id')};
-    User.setOwner(options, function (err){
-      if (err) return res.serverError()
+    User.setDefaults(options, function (err){
+      if (err) {
+        return res.serverError()
+      }
+
+      User.attachDefaultRole(options, function(err){
+        if (err) {
+          return res.serverError()
+        }
+
+      });
+
     });
-    User.attachDefaultRole(options, function(err){
-      if (err) return res.serverError()
-    });
-    return res.ok(200, options)
-  },
+    return res.ok()
+  }
 };
--- api/models/User.js
+++ api/models/User.js
@@ -36,15 +36,22 @@ module.exports = {
       return this.firstName + this.lastName
     }
   },
-  setOwner: function ( options, cb ) {
+  setDefaults: function ( options, cb ) {
     /**
       @param
         user - id of user model to update
     **/
-    User.update({ id: options.user }, { owner: options.user })
-      .catch(function(e){
-        return cb(e)
-      })
+    User.update({ id: options.user }, {
+      createdBy: options.user,
+      owner: options.user,
+      createdAt: new Date(),
+    }).exec(function after(err, updated){
+      if( err ){
+        return cb(err);
+      }
+
+      return cb();
+    });
 
     return cb();
   },
@@ -68,7 +75,6 @@ module.exports = {
       if( err || user == undefined ){
         return cb('error');
       }
-
       Role.findOne({ name: options.role})
       .exec(function(err, role){
         if( err || role == undefined){
@@ -76,11 +82,11 @@ module.exports = {
         }
 
         user.roles.add(role.id);
+
         user.save(function(err){
           if(err){
             return cb('error');
           }
-
           return cb();
         });
       });
