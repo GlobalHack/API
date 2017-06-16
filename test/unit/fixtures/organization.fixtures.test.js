f4022c2504d8835e2d47fb51cd056755ef6c1390
James Marlowe <jameskmarlowe@gmail.com>
2017-06-16 00:28:52 -0500

    fix test

--- test/unit/fixtures/organization.fixtures.test.js
+++ test/unit/fixtures/organization.fixtures.test.js
@@ -6,14 +6,8 @@ describe('Find Organization fixtures', function() {
     Organization.find()
       .then(function(results) {
         // run some tests
-        expect(results[0].name).to.equal("Karen House")
-        expect(results[1].name).to.equal("Missionaries of Charity")
-        expect(results[2].name).to.equal("New Life Evangelistic Center")
-        expect(results[3].name).to.equal("Sunshine Ministries Homeless Shelter")
-        expect(results[4].name).to.equal("The Bridge – Centenary Church")
-        expect(results[5].name).to.equal("Horizon Club")
-        expect(results[6].name).to.equal("St. Patrick Center")
-        expect(results[7].name).to.equal("The SPOT")
+        expect(results[0].name).to.equal("Biddle House Opportunities Center")
+        expect(results[1].name).to.equal("Covenant House")
         done()
       })
       .catch(done)
